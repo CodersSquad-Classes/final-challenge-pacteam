f4e5e669dbb5f190bc46ec5fab654467b91e0ee5
Sebastian Crossa <sebastiancrossa@hotmail.com>
2022-05-24 22:08:50 -0500

    feat: add level image to screen

--- main.go
+++ main.go
@@ -2,26 +2,35 @@ package main
 
 import (
 	"fmt"
+	_ "image/png"
+	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 type Game struct {
 }
 
+var level *ebiten.Image
+
 const (
 	/* Screen settings */
-	screenWidth  = 540
-	screenHeight = 540
+	screenWidth  = 650
+	screenHeight = 720
 )
 
 func init() {
-	fmt.Println("init")
+	var err error
+	level, _, err = ebitenutil.NewImageFromFile("assets/level-2.png")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 // *** Core Ebiten functions *** //
 func (g *Game) Draw(screen *ebiten.Image) {
-	fmt.Println("draw")
+	screen.DrawImage(level, nil)
 }
 
 func (g *Game) Update() error {
