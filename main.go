dc622ae5275d1b9f6a1c796533c7e76ff2d6747c
Sebastian Crossa <sebastiancrossa@hotmail.com>
2022-05-24 21:48:58 -0500

    feat: create base screen

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Game struct {
+	gopher *char
+}
+
+type char struct {
+	x  int
+	y  int
+	vx int
+	vy int
+}
+
+const (
+	/* Screen settings */
+	screenWidth  = 540
+	screenHeight = 540
+)
+
+func init() {
+	fmt.Println("init")
+}
+
+// *** Core Ebiten functions *** //
+func (g *Game) Draw(screen *ebiten.Image) {
+	fmt.Println("draw")
+}
+
+func (g *Game) Update() error {
+	fmt.Println("update")
+
+	return nil
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+// *** *** //
+
+func main() {
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Pacman by Pacteam")
+
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		panic(err)
+	}
+}
